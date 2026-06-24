49a93669a2973dd2f531241bb05cedeb77892d0e
Gotharikusuma <kusuma.gothari@gmail.com>
2026-06-24 10:24:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,32 +10,7 @@ def get_connection():
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
     return conn
-"""
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
 
-        conn = get_connection()
-        cursor = conn.cursor()
-
-        cursor.execute(
-            "SELECT * FROM users WHERE username=? AND password=?",
-            (username, password)
-        )
-
-        user = cursor.fetchone()
-        conn.close()
-
-        if user:
-            session['user'] = user['username']
-            return redirect('/')
-
-        return "Invalid Username or Password"
-
-    return render_template("login.html")
-"""
 # ---------------- HOME (DASHBOARD ONLY) ----------------
 @app.route('/')
 def home():
@@ -67,35 +42,7 @@ def home():
         total_subjects=total_subjects,
         avg_marks=avg_marks
     )
-    """
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    # total students
-    cursor.execute("SELECT COUNT(*) FROM students")
-    total_students = cursor.fetchone()[0]
-
-    # total marks entries
-    cursor.execute("SELECT COUNT(*) FROM marks")
-    total_marks = cursor.fetchone()[0]
-
-    # average marks
-    cursor.execute("SELECT AVG(marks) FROM marks")
-    avg_marks = cursor.fetchone()[0]
-
-    conn.close()
-
-    if avg_marks is None:
-        avg_marks = 0
-
-    return render_template(
-        "home.html",
-        total_students=total_students,
-        total_marks=total_marks,
-        avg_marks=avg_marks
-    )
-"""
-
+    
 # ---------------- ADD STUDENT ----------------
 @app.route('/add_student', methods=['GET', 'POST'])
 def add_student():
@@ -143,16 +90,6 @@ def view_students():
         cursor.execute(
               "SELECT * FROM students ORDER BY class ASC, name ASC"
         )
-
-    #students = cursor.fetchall()
-    #conn.close()
-
-    #return render_template(
-     #   "view_students.html",
-     #   students=students,
-     #   search=search
-
-    #)
     students = cursor.fetchall()
     conn.close()
 
@@ -249,26 +186,7 @@ def add_marks(id):
         return redirect('/view_students')
 
     return render_template("add_marks.html")
-    """
-    if request.method == 'POST':
-        subject = request.form['subject']
-        marks = request.form['marks']
-
-        conn = get_connection()
-        cursor = conn.cursor()
-
-        cursor.execute(
-            "INSERT INTO marks (student_id, subject, marks) VALUES (?, ?, ?)",
-            (id, subject, int(marks))
-        )
-
-        conn.commit()
-        conn.close()
-
-        return redirect('/view_students')
-
-    return render_template("add_marks.html")
-"""
+    
 # ---------------- STUDENT PERFORMANCE ----------------
 @app.route('/student/<int:id>')
 def student_detail(id):
@@ -321,36 +239,6 @@ def charts(id):
         marks=marks
     )
 
-"""
-@app.route('/search')
-def search():
-    query = request.args.get('query')
-
-    conn = sqlite3.connect("students.db")
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "SELECT * FROM students WHERE name LIKE ?",
-        (f"%{query}%",)
-    )
-
-    student = cursor.fetchone()
-    conn.close()
-
-    if student is None:
-        return render_template(
-            "students.html",
-            student=None,
-            message="No student found."
-        )
-
-    return render_template(
-        "students.html",
-        student=student,
-        message=None
-    )
-"""
 # ---------------- RUN ----------------
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
